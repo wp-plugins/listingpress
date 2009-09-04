f6908d1e3fea2f67d4b33fda59e25e2864127626
jasonbenesch <jasonbenesch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-04 05:14:21 +0000

    Upgrading
    git-svn-id: https://plugins.svn.wordpress.org/listingpress/tags/1.0.4@151935 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ listingpress.php
@@ -0,0 +1,125 @@
+<?php
+/***************************************************************************
+
+Plugin Name:  ListingPress
+Plugin URI:   http://www.listingpress.com/
+Description:  Easily embed Real Estate Listings into your blog.
+Version:      1.0.4
+Author:       Jason Benesch
+Author URI:   http://www.jasonbenesch.com/
+
+Copyright (C) 2008-2009 Jason Benesch
+	
+****************************************************************************
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*****************************************************************************
+
+	Many Thanks To:
+	
+	Mark James
+	http://www.famfamfam.com/lab/icons/silk/
+	- For allowing me to use his Silk icon set 1.3
+	
+	Adam Hupp
+	http://hupp.org/adam/
+	- For his awesome WP-FacebookConnect plugin
+	
+	Jonas Einarsson
+	http://jonas.einarsson.net/ajaxlogin
+	- For parts of his AJAX Login plugin
+	
+	Mahmoud Al-Qudsi
+	http://neosmart.net/dl.php?id=14
+	- For parts of the caching
+	
+	Viper 007
+	http://www.viper007bond.com/wordpress-plugins/vipers-video-quicktags/
+	- For shortcode and tinymce plugin
+	
+	Donncha O Caoimh
+	http://ocaoimh.ie/
+	- Way too much to mention. 
+	
+
+******************************************************************************/
+
+require_once( 'core/lp_registration.php' );
+require_once( 'core/lp_settings.php' );
+require_once( 'core/lp_activation.php' );
+
+if( lp_is_registered() ) {
+
+	$GLOBALS['lp_access_token'] = lp_access_token();
+	$GLOBALS['lp_current_version'] = lp_current_version();
+	$GLOBALS['lp_host'] = lp_host();
+	$GLOBALS['lp_feed'] = lp_feed();
+	$lpvars = get_option('ListingPressQuery');
+	
+	define( 'LP_ACCESS_TOKEN', lp_access_token() );
+	define( 'LP_HOST', lp_host() );
+	define( 'LP_FEED_ID', lp_feed() );
+	define( 'LP_PER_PAGE', $lpvars['lp_per_page'] );
+	define( 'LP_GMAP_API', $lpvars['lp_maps'] );
+	define( 'LP_ZILLOW_API', $lpvars['lp_zillow'] );
+	define( 'LP_EDU_API', $lpvars['lp_education'] );
+	define( 'LP_MENU_BAR', $lpvars['lp_menu_bar'] );
+
+	/* Core Files */
+	require_once( 'core/lp_cache.php' );
+	require_once( 'core/lp_query.php' );
+	require_once( 'core/lp_functions.php' );
+	require_once( 'core/lp_lookups.php' );
+	require_once( 'core/lp_rewrites.php' );
+	require_once( 'core/lp_template.php' );
+	
+	/* ListingPress User Authentication */
+	require_once( 'users/lp_user_auth.php' );
+	
+	/* Google Maps API */
+	if( defined('LP_GMAP_API') && constant('LP_GMAP_API') != '' ) {
+		require_once( 'google/lp_maps.php' );
+	}
+	
+	/* Zillow API */
+	if( defined('LP_ZILLOW_API') && constant('LP_ZILLOW_API') != '' ) {
+		require_once( 'zillow/zws_query.php' );
+		require_once( 'zillow/zws_functions.php' );
+		//require_once( 'zillow/zws_rewrites.php' );
+		//require_once( 'zillow/zws_template.php' );
+	}
+	
+	/* ListingPress Menu Bar */
+	if( defined('LP_MENU_BAR') && constant('LP_MENU_BAR') == 'show' ) {
+		require_once( 'menu/lp_menu_bar.php' );
+	}
+	
+	/* Facebook Connect API */
+	//require_once( 'connect/fbconnect.php' );
+	/* Facebook Connect API */
+	
+	/* Education API */
+	//require_once( 'education/edu_query.php' );
+	//require_once( 'education/edu_functions.php' );
+	/* Education API */
+	
+	/* ListingPress Shortcode */
+	//require_once( 'shortcode/lp_shortcode.php' );
+	/* ListingPress Shortcode */
+		
+}
+
+?>
